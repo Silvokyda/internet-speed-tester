ea0c5eaef42b3dee29aa98cd3b6fc5aaae0fc82e
Silvokyda <deseve48@gmail.com>
2024-05-12 17:37:25 +0300

    direct check

--- speedtestApp/utils.py
+++ speedtestApp/utils.py
@@ -1,4 +1,6 @@
 import speedtest
+import urllib.request
+import time
 
 def run_speed_test():
     st = speedtest.Speedtest()
@@ -11,3 +13,17 @@ def run_speed_test():
         'upload_speed': upload_speed,
         'ping': ping
     }
+
+def measure_speed(url, file_size):
+    time_start = time.time()
+    response = urllib.request.urlopen(url)
+    downloaded_bytes = 0
+    while True:
+        data_chunk = response.read(1024) 
+        if not data_chunk:
+            break
+        downloaded_bytes += len(data_chunk)
+    time_end = time.time()
+    print(f'Downloaded {downloaded_bytes} bytes in {time_end - time_start} seconds')
+    download_speed = (downloaded_bytes * 8) / (time_end - time_start) / 1024 / 1024  
+    return download_speed  
--- speedtestApp/views.py
+++ speedtestApp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from .utils import run_speed_test
+from .utils import run_speed_test, measure_speed
 from django.http import JsonResponse
+
+# def speed_test_view(request):
+#     if request.method == 'POST':
+#         results = run_speed_test()
+#         return JsonResponse(results)
+#     return render(request, 'test.html')
 
 def speed_test_view(request):
     if request.method == 'POST':
-        results = run_speed_test()
+        url = 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Tokyo_Sky_Tree_2012.JPG'  
+        file_size = 8185374
+        download_speed = measure_speed(url, file_size)
+        results = {'download_speed': download_speed}
         return JsonResponse(results)
     return render(request, 'test.html')
 
